baf9fef8a6baee78abd2d45fb0a8e3ee9a9838c4
Bryan Johnson <bjohnson@Bryans-Mac-mini.local>
2014-07-02 10:43:12 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Bryan Johnson on 7/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
